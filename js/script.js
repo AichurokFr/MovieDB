6b554ce6b00f5c7b7ff9c5b7a84db5fe92aa8505
Aichurok <kuttubekovnaaichurok174@gmail.com>
2022-07-06 10:48:04 +0600

    First commit

--- js/script.js
+++ js/script.js
@@ -8,3 +8,35 @@ const movieDB = {
     ]
 };
 
+const movieList = document.querySelector('.promo_interective-list')
+
+movieDB.movies.sort()
+
+
+function createMovieList() {
+    movieList.innerHTML = ''
+
+    movieDB.movies.forEach((film) => {
+        movieList.innerHTML += `   
+            <li class="promo__interactive-item">${film}
+                <div class="delete"></div>
+             </li>  
+        `
+    })
+}
+
+createMovieList()
+
+const addFom = document.querySelector('.add')
+const addInput = document.querySelector('.adding_input')
+
+addFom.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const newFilm = addInput.value 
+
+    movieDB.movies.push(newFilm)
+    movieDB.movies.sort()
+
+    createMovieList()
+})
